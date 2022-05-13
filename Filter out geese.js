c6537c918fb1d1b14fc574f72c911c8b5047774c
Richard Chin <itsknox@gmail.com>
2022-05-12 22:31:30 -0400

    Add files via upload

---
+++ Filter out geese.js
@@ -0,0 +1,9 @@
+
+
+
+function gooseFilter (birds) {
+    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+    // return an array containing all of the strings in the input array except those that match strings in geese
+    return birds.filter(b => !geese.includes(b))
+    
+  };
