e92e584ecd2db14fa54f2961ede73c5df575aa68
Richard Chin <itsknox@gmail.com>
2022-05-12 22:32:50 -0400

    Update Filter out geese.js

--- Filter out geese.js
+++ Filter out geese.js
@@ -1,3 +1,11 @@
+// Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the 'geese' removed.
+
+// The geese are any strings in the following array, which is pre-populated in your solution:
+
+//   ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
+// For example, if this array were passed as an argument:
+
+//  ["Mallard", "Hook Bill", "African", "Crested", "Pilgrim", "Toulouse", "Blue Swedish"]
 
 
 
